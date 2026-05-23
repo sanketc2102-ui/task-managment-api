3efe9788d0d4ef76af716eb1f0dd230799f7780a
Sanket Chavan <sanketc2102@gmail.com>
2026-05-23 13:58:06 +0530

    establish cors configuration

--- src/app.js
+++ src/app.js
@@ -1,10 +1,21 @@
 import express from "express";
 import taskRouter from "./routes/tasks.routes.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ limit: "16kb", extended: true }));
+
+// cors configuration
+app.use(
+  cors({
+    origin: "http://localhost5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Authorization", "Content-Type"],
+  }),
+);
 
 app.use("/tasks", taskRouter);
 
